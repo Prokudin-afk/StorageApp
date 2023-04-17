082803365a3385832155f6bedbac8114004589c6
young_protokaa42 <TOprokudinTO@bk.ru>
2023-04-17 23:35:31 +0500

    dump

--- dump.sql
+++ dump.sql
@@ -34,7 +34,7 @@ CREATE TABLE `equipment` (
   PRIMARY KEY (`id`),
   KEY `equipment_user_id_foreign` (`user_id`),
   CONSTRAINT `equipment_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,6 +43,7 @@ CREATE TABLE `equipment` (
 
 LOCK TABLES `equipment` WRITE;
 /*!40000 ALTER TABLE `equipment` DISABLE KEYS */;
+INSERT INTO `equipment` VALUES (1,2,'router',7500.00,'12345','12345678','2023-04-17 10:32:31','2023-04-17 10:32:31');
 /*!40000 ALTER TABLE `equipment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -63,7 +64,7 @@ CREATE TABLE `logs` (
   PRIMARY KEY (`id`),
   KEY `logs_user_id_foreign` (`user_id`),
   CONSTRAINT `logs_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,7 +73,7 @@ CREATE TABLE `logs` (
 
 LOCK TABLES `logs` WRITE;
 /*!40000 ALTER TABLE `logs` DISABLE KEYS */;
-INSERT INTO `logs` VALUES (1,'add_equipment','',1,'2023-04-11 09:43:31','2023-04-11 09:43:31'),(2,'add_equipment','{\"attributes\":{},\"request\":{},\"query\":{},\"server\":{},\"files\":{},\"cookies\":{},\"headers\":{}}',1,'2023-04-11 09:44:11','2023-04-11 09:44:11'),(3,'add_equipment','null',1,'2023-04-11 09:45:11','2023-04-11 09:45:11'),(4,'add_equipment','null',1,'2023-04-11 09:45:36','2023-04-11 09:45:36'),(5,'add_equipment','null',1,'2023-04-11 09:45:46','2023-04-11 09:45:46'),(6,'add_equipment','\"1\"',1,'2023-04-11 09:48:10','2023-04-11 09:48:10'),(7,'add_equipment','{\"name\":\"1\",\"cost\":\"2\",\"serial_num\":null,\"inventory_num\":null}',1,'2023-04-11 09:58:00','2023-04-11 09:58:00'),(8,'add_equipment','{\"name\":\"1\",\"cost\":\"2\",\"serial_num\":\"3\",\"inventory_num\":\"4\"}',1,'2023-04-11 09:58:36','2023-04-11 09:58:36');
+INSERT INTO `logs` VALUES (1,'add_equipment','{\"name\":\"\\u041a\\u043e\\u043c\\u043c\\u0443\\u0442\\u0430\\u0442\\u043e\\u0440\",\"cost\":\"7500\",\"serial_num\":\"12345\",\"inventory_num\":\"12345678\"}',2,'2023-04-17 10:32:31','2023-04-17 10:32:31');
 /*!40000 ALTER TABLE `logs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,7 +89,7 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,7 +98,7 @@ CREATE TABLE `migrations` (
 
 LOCK TABLES `migrations` WRITE;
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-INSERT INTO `migrations` VALUES (1,'2019_12_14_000001_create_personal_access_tokens_table',1),(2,'2023_04_11_080420_create_permissions_table',1),(3,'2023_04_11_080439_create_roles_table',1),(4,'2023_04_11_080504_create_role_permissions_table',1),(5,'2023_04_11_080529_create_warehouses_table',1),(6,'2023_04_11_080606_create_users_table',1),(7,'2023_04_11_080615_create_logs_table',1),(8,'2023_04_11_080636_create_equipment_table',1),(9,'2023_04_11_080649_create_storages_table',1);
+INSERT INTO `migrations` VALUES (1,'2019_12_14_000001_create_personal_access_tokens_table',1),(2,'2023_04_11_080420_create_permissions_table',1),(3,'2023_04_11_080439_create_roles_table',1),(4,'2023_04_11_080504_create_role_permissions_table',1),(5,'2023_04_11_080529_create_warehouses_table',1),(6,'2023_04_11_080606_create_users_table',1),(7,'2023_04_11_080615_create_logs_table',1),(8,'2023_04_11_080636_create_equipment_table',1),(9,'2023_04_11_080649_create_storages_table',1),(10,'2023_04_13_145325_create_user_tokens_table',1);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -114,7 +115,7 @@ CREATE TABLE `permissions` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -123,7 +124,7 @@ CREATE TABLE `permissions` (
 
 LOCK TABLES `permissions` WRITE;
 /*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
-INSERT INTO `permissions` VALUES (1,'addEquipment','2023-04-11 08:17:09','2023-04-11 08:17:09'),(2,'showEquipment','2023-04-11 08:17:46','2023-04-11 08:17:46'),(3,'editEquipment','2023-04-11 08:18:01','2023-04-11 08:18:01'),(4,'deleteEquipment','2023-04-11 08:18:11','2023-04-11 08:18:11'),(5,'showUserEquipment','2023-04-11 08:56:33','2023-04-11 08:56:33'),(6,'showAllEquipment','2023-04-11 08:56:59','2023-04-11 08:56:59'),(7,'moveEquipment','2023-04-11 08:57:38','2023-04-11 08:57:38'),(8,'showMovedEquipment','2023-04-11 08:58:29','2023-04-11 08:58:29');
+INSERT INTO `permissions` VALUES (1,'addEquipment','2023-04-13 18:02:23','2023-04-13 18:02:23'),(2,'showUserEquipment','2023-04-13 18:05:13','2023-04-13 18:05:13'),(3,'moveEquipment','2023-04-17 18:13:52','2023-04-17 18:13:52'),(4,'showMovedEquipment','2023-04-17 18:20:30','2023-04-17 18:20:30');
 /*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -178,7 +179,7 @@ CREATE TABLE `role_permissions` (
   KEY `role_permissions_permission_id_foreign` (`permission_id`),
   CONSTRAINT `role_permissions_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`),
   CONSTRAINT `role_permissions_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -187,7 +188,7 @@ CREATE TABLE `role_permissions` (
 
 LOCK TABLES `role_permissions` WRITE;
 /*!40000 ALTER TABLE `role_permissions` DISABLE KEYS */;
-INSERT INTO `role_permissions` VALUES (1,2,1,'2023-04-11 09:04:49','2023-04-11 09:04:49'),(2,2,5,'2023-04-11 09:08:00','2023-04-11 09:08:00'),(3,3,2,'2023-04-11 09:09:01','2023-04-11 09:09:01'),(4,3,6,'2023-04-11 09:09:06','2023-04-11 09:09:06'),(5,3,7,'2023-04-11 09:09:34','2023-04-11 09:09:34'),(6,3,8,'2023-04-11 09:09:38','2023-04-11 09:09:38');
+INSERT INTO `role_permissions` VALUES (1,1,1,'2023-04-13 18:03:58','2023-04-13 18:03:58'),(2,1,2,'2023-04-17 15:42:40','2023-04-17 15:42:40'),(3,2,3,'2023-04-17 18:17:59','2023-04-17 18:17:59'),(4,2,4,'2023-04-17 18:20:52','2023-04-17 18:20:52');
 /*!40000 ALTER TABLE `role_permissions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -204,7 +205,7 @@ CREATE TABLE `roles` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -213,7 +214,7 @@ CREATE TABLE `roles` (
 
 LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,'admin','2023-04-11 08:59:57','2023-04-11 08:59:57'),(2,'provider','2023-04-11 09:00:42','2023-04-11 09:00:42'),(3,'manager','2023-04-11 09:01:20','2023-04-11 09:01:20');
+INSERT INTO `roles` VALUES (1,'provider','2023-04-13 16:12:03','2023-04-13 16:12:03'),(2,'manager','2023-04-17 18:17:26','2023-04-17 18:17:26');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -251,6 +252,35 @@ LOCK TABLES `storages` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `user_tokens`
+--
+
+DROP TABLE IF EXISTS `user_tokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_tokens` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_tokens_user_id_foreign` (`user_id`),
+  CONSTRAINT `user_tokens_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_tokens`
+--
+
+LOCK TABLES `user_tokens` WRITE;
+/*!40000 ALTER TABLE `user_tokens` DISABLE KEYS */;
+INSERT INTO `user_tokens` VALUES (19,2,'hoLrOwrLLlD','2023-04-17 13:12:01','2023-04-17 13:12:01');
+/*!40000 ALTER TABLE `user_tokens` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -276,7 +306,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'provider','123',2,'2023-04-11 09:26:22','2023-04-11 09:26:22'),(2,'manager','123',3,'2023-04-11 09:28:08','2023-04-11 09:28:08');
+INSERT INTO `users` VALUES (2,'provider','123',1,'2023-04-13 16:12:05','2023-04-13 16:12:05');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -293,7 +323,7 @@ CREATE TABLE `warehouses` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -302,7 +332,6 @@ CREATE TABLE `warehouses` (
 
 LOCK TABLES `warehouses` WRITE;
 /*!40000 ALTER TABLE `warehouses` DISABLE KEYS */;
-INSERT INTO `warehouses` VALUES (1,'55.17965 61.37263','2023-04-11 09:22:37','2023-04-11 09:22:37'),(2,'55.1602 61.28922','2023-04-11 09:23:37','2023-04-11 09:23:37'),(3,'55.18668 61.2742','2023-04-11 09:24:14','2023-04-11 09:24:14');
 /*!40000 ALTER TABLE `warehouses` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -315,4 +344,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-11 20:40:12
+-- Dump completed on 2023-04-17 23:35:18
